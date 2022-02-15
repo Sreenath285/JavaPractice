aece2ea228b83564acf862754eef6e9ffba2701c
Sreenath285 <sreenath.285@gmail.com>
2022-02-15 18:10:16 +0530

    Part time employee & wage

--- EmpWage/src/javapractice/EmpWage.java
+++ EmpWage/src/javapractice/EmpWage.java
@@ -2,18 +2,23 @@ package javapractice;
 
 public class EmpWage {
 	
-	 public static void main(String[] args) {
-	        int IS_FULL_TIME = 1;
-	        int WAGE_PER_HOUR = 20;
-	        int empHrs = 0, empWage = 0;
-	        double empCheck = Math.floor(Math.random() * 10) % 2;
-	        if (empCheck == IS_FULL_TIME) {
-	            empHrs = 8;
-	        }
-	        else {
-	            empHrs = 0;
-	        }
-	        empWage = empHrs * WAGE_PER_HOUR;
-	        System.out.println("Employee Wage : " + empWage);
-	    }
+	public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int WAGE_PER_HOUR = 20;
+        int empHrs = 0, empWage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+        }
+        else if (empCheck == IS_PART_TIME){
+            empHrs = 4;
+        }
+        else {
+            empHrs = 0;
+        }
+        empWage = empHrs * WAGE_PER_HOUR;
+        System.out.println("Employee Wage : " + empWage);
+
+    }
 }
