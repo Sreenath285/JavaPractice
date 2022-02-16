41d9a195fbfdf9a91f3830604d7990fc6f4530be
Sreenath285 <sreenath.285@gmail.com>
2022-02-16 16:10:13 +0530

    checking employee present or absent

---
+++ EmpWageBuilder/src/EmpPresentOrAbsent.java
@@ -0,0 +1,14 @@
+
+public class EmpPresentOrAbsent {
+
+	public int employeeAttendance() {
+        int isPresent = (int) Math.floor(Math.random() * 10) % 2;
+        if (isPresent == 1) {
+        	System.out.println("Employee is present");
+        }
+        else {
+        	System.out.println("Employee is absent");
+        }
+        return isPresent;
+    }
+}
